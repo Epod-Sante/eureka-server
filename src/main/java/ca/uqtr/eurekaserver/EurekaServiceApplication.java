ffc3ec01b88333e8413e7ccb1a4b944c51e5695d
zinnourlahcene <zinnour@uqtr.ca>
2020-02-16 19:28:25 -0500

    ubuntu

--- src/main/java/ca/uqtr/eurekaserver/EurekaServiceApplication.java
+++ src/main/java/ca/uqtr/eurekaserver/EurekaServiceApplication.java
@@ -1,5 +1,7 @@
 package ca.uqtr.eurekaserver;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
